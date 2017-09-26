82c7d03c55983402e26a220c3643d68236e0c787
Jasmine Wynn <jasminewynn95@gmail.com>
2017-09-26 15:44:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+  console.log(HELLO)
+}
